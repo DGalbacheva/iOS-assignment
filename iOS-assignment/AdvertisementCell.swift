130879219caca7579863a7391200b848e19633d3
Doroteya Galbacheva <galbachevva@gmail.com>
2025-06-16 13:29:56 +0300

    Added: AdvertisementCell, AdvertisementViewController, AdvertisementViewModel

---
+++ iOS-assignment/AdvertisementCell.swift
@@ -0,0 +1,8 @@
+//
+//  AdvertisementCell.swift
+//  iOS-assignment
+//
+//  Created by MacBook Pro 15 on 16.06.2025.
+//
+
+import Foundation
---
+++ iOS-assignment/AdvertisementsViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  AdvertisementsViewModel.swift
+//  iOS-assignment
+//
+//  Created by MacBook Pro 15 on 16.06.2025.
+//
+
+import Foundation
